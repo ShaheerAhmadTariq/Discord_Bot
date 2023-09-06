68f642f700d449518650a90488ae53f6dea49953
shaheer ahmad <91483870+ShaheerAhmadTariq@users.noreply.github.com>
2023-09-06 23:08:32 +0500

    Update start.bash

--- start.bash
+++ start.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-python download_model.py PygmalionAI/pygmalion-6b --output models
-python server.py --api --chat --model PygmalionAI_pygmalion-6b --model-dir models
+python download_model.py PygmalionAI/pygmalion-2-13b --output models
+python server.py --api --chat --model PygmalionAI_pygmalion-2-13b --model-dir models
 
