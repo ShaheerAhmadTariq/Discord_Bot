49fa2dd9353f84f25479b86de43f3742378dbd8e
mujee7 <mujtabarehman1444@gmail.com>
2023-09-19 01:08:05 +0500

    updated bot

--- index.js
+++ index.js
@@ -3,26 +3,27 @@ require("dotenv").config();
 const ffmpeg = require('fluent-ffmpeg');
 const axios = require('axios');
 const stream = require('stream');
+const fs = require('fs');
 
 const Token = process.env.TELEGRAM_TOKEN;
 const bot = new telegramBot(Token, { polling: true });
 
-bot.on('text', (message) => {
-  console.log(message);
-  bot.sendMessage(message.from.id, "Yo, what's up bro");
-});
+// bot.on('text', (message) => {
+//   console.log(message);
+//   bot.sendMessage(message.from.id, "Yo, what's up bro");
+// });
 
-bot.onText(/\/echo (.+)/,(msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
+// bot.onText(/\/echo (.+)/,(msg, match) => {
+//   // 'msg' is the received Message from Telegram
+//   // 'match' is the result of executing the regexp above on the text content
+//   // of the message
 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+//   const chatId = msg.chat.id;
+//   const resp = match[1]; // the captured "whatever"
 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-})
+//   // send back the matched "whatever" to the chat
+//   bot.sendMessage(chatId, resp);
+// })
 
 bot.setMyCommands([
   { command: "start", description: "إبدأ من جديد" },
@@ -33,48 +34,73 @@ bot.setMyCommands([
 
 bot.on('voice', async (message) => {
   try {
+    console.log(message)
     const voiceId = message.voice.file_id;
+    const username = message.from.username;
+    const userId = message.from.id
+    const chatId = message.chat.id
 
-    // Download the OGG audio file using the bot's getFile method
-    const oggAudioFile = await bot.getFile(voiceId);
-
-    // Construct the URL to download the file
-    const oggAudioUrl = `https://api.telegram.org/file/bot${Token}/${oggAudioFile.file_path}`;
-
-    // Create a PassThrough stream for the converted WAV audio
-    const wavAudioStream = ffmpeg()
-      .input(oggAudioUrl)
-      .audioCodec('pcm_s16le') // Set the audio codec to WAV
-      .toFormat('wav')
-      .pipe();
-
-    // Convert the PassThrough stream to a Node.js Readable Stream
-    const wavReadableStream = new stream.PassThrough();
-    wavAudioStream.pipe(wavReadableStream);
-
-    // Read the WAV audio data and send it to your API endpoint as base64
-    let wavAudioData = '';
-    wavReadableStream.on('data', (chunk) => {
-      wavAudioData += chunk.toString('base64');
-    });
-
-    wavReadableStream.on('end', () => {
-      // Send the WAV audio data to your API endpoint
-      sendWavToApi(wavAudioData);
-    });
+    const oggAudioUrl = `https://api.telegram.org/file/bot${Token}/${voiceId}`;
+    const oggFileName = `${username}${Date.now()}.ogg`
+    fs.writeFileSync( `../${oggFileName}`, oggAudioUrl);
+    
+    const response= await sendVoiceToApi(oggFileName, userId, username);
+    bot.sendMessage(chatId,response)
     
   } catch (err) {
     console.error(err.message);
   }
 });
 
-function sendWavToApi(wavAudioData) {
+
+
+
+
+bot.on('text', async(message) => {
+
+  const username = message.from.username;
+    const userId = message.from.id
+    const chatId = message.chat.id
+
+  console.log(message)
+  // Check if the message starts with the bot's prefix or is a slash command
+  if ( message.text.startsWith('/')) {
+    console.log("worked")
+    return; // Ignore messages with commands
+  }
+
+  const response= await sendTextToApi(message.text, userId, username);
+  bot.sendMessage(chatId,response)
+  // Your bot's logic for responding to regular messages goes here
+  // For example, you can reply with a message:
+  
+
+  // Or perform any other actions based on the message content
+});
+
+
+async function sendTextToApi(message, userId, username) {
+  try{
+
+    const response =await axios.post("https://3b32-139-135-45-18.ngrok-free.app/generate_response_llm", { message: message, user_id: userId.toString(), user_name: username  })
+    return (response.data.message)
+  } catch(err){
+    console.error('Error sending audio data to the API:', err)
+
+  }
   // Use axios to send the audio data directly to your API
-  axios.post("http://localhost:1000/upload-wav", { audioData: wavAudioData })
-    .then((response) => {
-      console.log('Response from API:', response.data);
-    })
-    .catch((error) => {
-      console.error('Error sending audio data to the API:', error);
-    });
+ 
+}
+
+async function sendVoiceToApi(filePath, userId, username) {
+  try{
+
+    const response =await axios.post("https://3b32-139-135-45-18.ngrok-free.app/generate_response_llm_audio", { audio_file_path: filePath, user_id: userId.toString(), user_name: username  })
+    return (response.data.message)
+  } catch(err){
+    console.error('Error sending audio data to the API:', err)
+
+  }
+  // Use axios to send the audio data directly to your API
+ 
 }
