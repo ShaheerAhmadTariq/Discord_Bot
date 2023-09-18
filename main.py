3e7fa3e266316e3ffad8b29860bbdfbe197c65a3
shaheer <shaheerahmadtariq@gmail.com>
2023-09-18 23:14:35 +0500

    Fix: starting

--- main.py
+++ main.py
@@ -29,19 +29,3 @@ async def generate_response_llm(message: Message):
     # except Exception as e:
     #     print("Error is", e)
     #     return "error in backend"
-
-class MessageGPT(BaseModel):
-    message: str
-    user_id: str
-    user_name: str
-@app.post("/generate_response_chatgpt")
-async def generate_response_chatgpt(message: MessageGPT):
-    try:
-        print("message: ", message)
-        response = await get_chain_response(message.user_id, message.message, message.user_name)
-        # response = "hello from chatgpt"
-        # await asyncio.sleep(10)
-        return response
-    except Exception as e:
-        print("Error is", e)
-        return "error in backend"
